10f7f580ba8674e10b017e28c38685c7292a6377
bblezek <Brin.blezek@trojans.dsu.edu>
2020-10-08 20:20:36 -0500

    - Added initial version of run time tests

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,53 +1,56 @@
 package com.company;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 public class Main {
 
     //Natural Fibonacci function
-    public static long fibRecur(long num) {
+    public static long fibRecur(long x) {
         //If we have reached "the bottom", return 0 or 1
-        if (num == 0 || num == 1) {
-            return num;
+        if (x == 0 || x == 1) {
+            return x;
         } else {
-            //Otherwise move "down" the tree, recrusively discovering and adding elements
-            return fibRecur(num - 1) + fibRecur(num - 2);
+            //Otherwise move "down" the tree, recursively discovering and adding elements
+            return fibRecur(x - 1) + fibRecur(x - 2);
         }
     }
 
     //Fibonacci function with cache
-    public static long fibCache(long num) {
+    public static long fibCache(long x) {
         //Creating a table to hold previously calculated numbers
-        long[] lookupTable = new long[(int) num + 1];
-        return fibCacheHelper(num, lookupTable);
+        long[] lookupTable = new long[(int) x + 1];
+        return fibCacheHelper(x, lookupTable);
     }
 
     //Fibonacci cache function helper function
     //Takes the index of the number we are trying to find and a table that will store elements in the sequence
-    public static long fibCacheHelper(long num, long[] table) {
+    public static long fibCacheHelper(long x, long[] table) {
         //If we are "at the bottom"
-        if (num == 0 || num == 1) {
-            return num;
-        //If the number at the index has already been calculated
-        } else if (table[(int) num] != 0) {
-            return table[(int) num];
-        //If the number at the index has not been calculated, calculate it and add to the table
+        if (x == 0 || x == 1) {
+            return x;
+            //If the number at the index has already been calculated
+        } else if (table[(int) x] != 0) {
+            return table[(int) x];
+            //If the number at the index has not been calculated, calculate it and add to the table
         } else {
-            table[(int) num] = fibCacheHelper(num - 1, table) + fibCacheHelper(num - 2, table);
-            return table[(int) num];
+            table[(int) x] = fibCacheHelper(x - 1, table) + fibCacheHelper(x - 2, table);
+            return table[(int) x];
         }
     }
 
     //Fibonacci function with loop
-    public static long fibLoop(long num) {
+    public static long fibLoop(long x) {
         long A = 0;
         long B = 1;
         long next;
         //If we are looking for one of the first two elements in the sequence
-        if (num < 2) {
-            return num;
+        if (x < 2) {
+            return x;
         } else {
             //If it is not one of the first few elements,
             //we begin at the bottom and calculate up
-            for (int i = 2; i <= num; i++) {
+            for (int i = 2; i <= x; i++) {
                 next = A + B;
                 A = B;
                 B = next;
@@ -95,7 +98,7 @@ public class Main {
     }
 
     //Fibonacci sequence function based on matrices
-    public static long fibMatrix(long num) {
+    public static long fibMatrix(long x) {
 //This matrix will store our "powers" as we calculate them
         long[][] powerMatrix = {{1, 1},
                 {1, 0}};
@@ -107,11 +110,11 @@ public class Main {
         //This is because the first value of the sequence is 0 and the second value is 1, so we can't technically
         //perform a matrix multiplication for those two "powers" and thus the sequence of matrix powers starts at
         //index 3 with "M to the second"
-        long y = num - 2;
+        long y = x - 2;
         //Return 0 if number equals 0
-        if (num == 0) {
-            return num;
-        //Otherwise, iterate over "binary digits" of num to find result matrix
+        if (x == 0) {
+            return x;
+            //Otherwise, iterate over "binary digits" of num to find result matrix
         } else {
             while (y > 0) {
                 //If "binary" digit at lowest position is equal to 1
@@ -132,17 +135,17 @@ public class Main {
     //Tests to ensure that functions are working
     public static boolean verificationTests() {
         long recurResult, cacheResult, loopResult, matrixResult;
-        for (long num = 0; num < 40; num++) {
-            recurResult = fibRecur(num);
-            System.out.printf("%d element in Fibonacci sequence is %d \t", num, recurResult);
-            cacheResult = fibCache(num);
+        for (long x = 0; x < 10; x++) {
+            recurResult = fibRecur(x);
+            System.out.printf("%d element in Fibonacci sequence is %d \t", x, recurResult);
+            cacheResult = fibCache(x);
             System.out.printf("%d \t", cacheResult);
-            loopResult = fibLoop(num);
+            loopResult = fibLoop(x);
             System.out.printf("%d \t", loopResult);
-            matrixResult = fibMatrix(num);
+            matrixResult = fibMatrix(x);
             System.out.printf("%d \n", matrixResult);
             if (recurResult == cacheResult && cacheResult == loopResult && loopResult == matrixResult) {
-                System.out.printf("Result for %d is correct! \n", num);
+                System.out.printf("Result for %d is correct! \n", x);
             } else {
                 return false;
             }
@@ -150,10 +153,86 @@ public class Main {
         return true;
     }
 
+    //Function to retrieve CPU time
+    public static long getCpuTime() {
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        return bean.isCurrentThreadCpuTimeSupported() ?
+                bean.getCurrentThreadCpuTime() : 0L;
+    }
+
+    public static void runTimeTests() {
+        //Max time to run is 4 minutes
+        long maxTime = 240000000000L;
+        long beforeTime, afterTime;
+        long recurPrevTime = 0;
+        long loopPrevTime = 0;
+        long cachePrevTime = 0;
+        long matrixPrevTime = 0;
+
+        //Printing header row for recursive and loop function tables
+        System.out.printf("%7s %85s \n", "Fib Recur", "Fib Loop");
+        System.out.printf("%7s %15s %31s %15s %15s %15s", "N", "X", "Fib(x)", "Time", "Doubling", "Expected");
+        System.out.printf("%7s %15s %31s %15s %15s %15s \n", "N", "X", "Fib(x)", "Time", "Doubling", "Expected");
+        System.out.printf("%71s %15s", "Ratio", "Doubling");
+        System.out.printf("%71s %15s \n", "Ratio", "Doubling");
+        System.out.printf("%87s", "Ratio");
+        System.out.printf("%87s\n", "Ratio");
+        long result = 0;
+        int N;
+        long x;
+        for (N = 1; N < 32; N++) {
+            for (x = (long) Math.pow(2, (double) N - 1); x < Math.pow(2, (double) N) - 1 && x < 93; x++) {
+                if (recurPrevTime < maxTime) {
+                    beforeTime = getCpuTime();
+                    result = fibRecur(x);
+                    afterTime = getCpuTime();
+                    System.out.printf("%7d %15d %31d %15d %15.3f %15.3f ", N, x, result, afterTime - beforeTime,
+                            (float) recurPrevTime / (afterTime - beforeTime), Math.pow(2, (x / 2)) / Math.pow(2, ((x - 1) / 2)));
+                    recurPrevTime = afterTime - beforeTime;
+                } else {
+                    System.out.printf("%7s %15s %31s %15s %15s %15s ", "na", "na", "na", "na", "na", "na", "na");
+                }
+                beforeTime = getCpuTime();
+                result = fibLoop(x);
+                afterTime = getCpuTime();
+                System.out.printf("%7d %15d %31d %15d %15.3f %15.3f ", N, x, result, afterTime - beforeTime,
+                        (float) loopPrevTime / (afterTime - beforeTime), (float) x / (x - 1));
+                loopPrevTime = afterTime - beforeTime;
+                System.out.println();
+            }
+        }
+
+        System.out.printf("%7s %87s \n", "Fib Cache", "Fib Matrix");
+        System.out.printf("%7s %15s %15s %15s %15s %15s", "N", "X", "Fib(x)", "Time", "Doubling", "Expected");
+        System.out.printf("%7s %15s %15s %15s %15s %15s \n", "N", "X", "Fib(x)", "Time", "Doubling", "Expected");
+        System.out.printf("%71s %15s", "Ratio", "Doubling");
+        System.out.printf("%71s %15s \n", "Ratio", "Doubling");
+        System.out.printf("%87s", "Ratio");
+        System.out.printf("%87s\n", "Ratio");
+        for (N = 1; N < 32; N++) {
+            for (x = (long) Math.pow(2, (double) N - 1); x < Math.pow(2, (double) N) - 1 && x < 93; x++) {
+                beforeTime = getCpuTime();
+                result = fibCache(x);
+                afterTime = getCpuTime();
+                System.out.printf("%7d %15d %31d %15d %15.3f %15.3f ", N, x, result, afterTime - beforeTime,
+                        (float) cachePrevTime / (afterTime - beforeTime), Math.pow(2, (x / 2)) / Math.pow(2, ((x - 1) / 2)));
+                cachePrevTime = afterTime - beforeTime;
+                beforeTime = getCpuTime();
+                result = fibMatrix(x);
+                afterTime = getCpuTime();
+                System.out.printf("%7d %15d %31d %15d %15.3f %15.3f ", N, x, result, afterTime - beforeTime,
+                        (float) matrixPrevTime / (afterTime - beforeTime), (float) x / (x - 1));
+                matrixPrevTime = afterTime - beforeTime;
+                System.out.println();
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         if (verificationTests()) {
             System.out.printf("Tests passed !\n");
         }
-
+        runTimeTests();
     }
 }
